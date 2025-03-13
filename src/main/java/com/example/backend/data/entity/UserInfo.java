a9a87b2f181c83a796d368e3c563c61e4b66460e
Kristiyan Rulev <rulevkristian@gmail.com>
2025-03-13 22:05:02 +0200

    added jwt service, auth filter, application and security configuration classes

--- src/main/java/com/example/backend/data/entity/UserInfo.java
+++ src/main/java/com/example/backend/data/entity/UserInfo.java
@@ -2,6 +2,11 @@ package com.example.backend.data.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
@@ -9,7 +14,7 @@ import lombok.*;
 @NoArgsConstructor
 @Getter
 @Setter
-public class UserInfo {
+public class UserInfo implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,9 +28,47 @@ public class UserInfo {
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
+    @Column(unique = true, length = 100, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
     @OneToOne(mappedBy = "userInfo")
     private Student student;
 
     @OneToOne(mappedBy = "userInfo")
     private Teacher teacher;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/example/backend/data/repository/UserInfoRepository.java
+++ src/main/java/com/example/backend/data/repository/UserInfoRepository.java
@@ -4,6 +4,9 @@ import com.example.backend.data.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
+    Optional<UserInfo> findByEmail(String email);
 }
