81504b3a9f1c700003f58c4bab74b30123d03735
Kristiyan Rulev <rulevkristian@gmail.com>
2025-05-10 23:32:36 +0300

    Added new test to department service

--- src/main/java/com/example/backend/data/repository/DepartmentRepository.java
+++ src/main/java/com/example/backend/data/repository/DepartmentRepository.java
@@ -1,7 +1,6 @@
 package com.example.backend.data.repository;
 
 import com.example.backend.data.entity.Department;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +10,5 @@ import java.util.Optional;
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
     Optional<Department> findByName(String name);
 
-    boolean existsByName(@NotBlank(message = "Department name is required") String departmentName);
+    boolean existsByName(String departmentName);
 }
